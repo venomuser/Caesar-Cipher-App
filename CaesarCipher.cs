b3edb7a417e32d0fae17b7d9d42cf8e38bcfa467
venomuser <sajjadmohammadifar96@gmail.com>
2024-02-14 00:37:28 +0330

    CaesarCipherApp is committed completely

---
+++ CaesarCipher.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipherCompleteApp
+{
+    internal class CaesarCipher
+    {
+        private char[] CapitalMainText = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+        private char[] SmallMainText = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        private char[] CapitalCeaserCipherText = { 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W' };
+        private char[] SmallCeaserCipherText = { 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w' };
+        
+        
+
+        public string Encrypt(string Text)
+        {
+            string encryptedText = string.Empty;
+            int index;
+            char[] txt=Text.ToCharArray();
+            foreach(char c in txt)
+            {
+                if (Char.IsUpper(c))
+                {
+                    index=Array.IndexOf(CapitalMainText,c);
+                    encryptedText += CapitalCeaserCipherText.GetValue(index).ToString();
+                }
+                else if(Char.IsLower(c))
+                {
+                    index = Array.IndexOf(SmallMainText, c);
+                    encryptedText += SmallCeaserCipherText.GetValue(index).ToString();
+                }
+                else
+                {
+                    encryptedText += c;
+                }
+            }
+            return encryptedText;
+        }
+
+        public string Decrypt(string Text)
+        {
+            string decryptedText = string.Empty;
+            int index;
+            char[] txt = Text.ToCharArray();
+            foreach (char c in txt)
+            {
+                if (Char.IsUpper(c))
+                {
+                    index=Array.IndexOf(CapitalCeaserCipherText,c);
+                    decryptedText += CapitalMainText.GetValue(index).ToString();
+                }
+                else if (Char.IsLower(c))
+                {
+                    index = Array.IndexOf(SmallCeaserCipherText, c);
+                    decryptedText += SmallMainText.GetValue(index).ToString();
+                }
+                else { decryptedText += c; }
+            }
+            return decryptedText;
+        }
+
+        public string Encrypt(string Text,int Shift)
+        {
+           Start:
+            try
+            {
+                string encryptedText = string.Empty;
+                int index;
+                char[] txt = Text.ToCharArray();
+                foreach (char c in txt)
+                {
+                    if (Char.IsUpper(c))
+                    {
+                        index = Array.IndexOf(CapitalMainText, c) - Shift + 3;
+                        if (index < 0)
+                        {
+                            index = 26 - Shift + 3;
+                        }
+                        else if (index > 25)
+                        {
+                            index = -1 - Shift + 3;
+                        }
+                        encryptedText += CapitalCeaserCipherText.GetValue(index).ToString();
+                    }
+                    else if (Char.IsLower(c))
+                    {
+                        index = Array.IndexOf(SmallMainText, c) - Shift + 3;
+                        if (index < 0)
+                        {
+                            index = 26 - Shift;
+                        }
+                        else if (index > 25)
+                        {
+                            index = -1 - Shift;
+                        }
+                        encryptedText += SmallCeaserCipherText.GetValue(index).ToString();
+                    }
+                    else
+                    {
+                        encryptedText += c;
+                    }
+                }
+                return encryptedText;
+            }
+            catch(Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message+" The default shift will be considered 3");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("===================================================================");
+                Shift = 3;
+                goto Start;
+            }
+        }
+        
+
+        public string Decrypt(string Text,int Shift)
+        {
+            Start:
+            try
+            {
+                string decryptedText = string.Empty;
+                int index;
+                char[] txt = Text.ToCharArray();
+                foreach (char c in txt)
+                {
+                    if (Char.IsUpper(c))
+                    {
+                        index = Array.IndexOf(CapitalCeaserCipherText, c) + Shift - 3;
+                        if (index < 0)
+                        {
+                            index = 26 + Shift - 3;
+                        }
+                        else if (index > 25)
+                        {
+                            index = -1 + Shift - 3;
+                        }
+                        decryptedText += CapitalMainText.GetValue(index).ToString();
+                    }
+                    else if (Char.IsLower(c))
+                    {
+                        index = Array.IndexOf(SmallCeaserCipherText, c) + Shift - 3;
+                        if (index < 0)
+                        {
+                            index = 26 + Shift - 3;
+                        }
+                        else if (index > 25)
+                        {
+                            index = -1 + Shift - 3;
+                        }
+                        decryptedText += SmallMainText.GetValue(index).ToString();
+                    }
+                    else { decryptedText += c; }
+                }
+                return decryptedText;
+            }
+            catch(Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message + " The default shift will be considered 3");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("===================================================================");
+                Shift = 3;
+                goto Start;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System.IO;
+namespace CaesarCipherCompleteApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception:
+            try
+            {
+            Start:
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Hello, Welcome to File Encryptor and Decryptor:");
+                Console.WriteLine("Choose your operation by pressing E, D, or F");
+                Console.WriteLine("(E) to Encrypt a message.");
+                Console.WriteLine("(D) to Decrypt a file.");
+                Console.WriteLine("(F) to exit.");
+                Console.ForegroundColor = ConsoleColor.White;
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.KeyChar == 'e' || key.KeyChar == 'E')
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nWelcome to encryption!");
+                    Console.WriteLine("Enter your message to encrypt:");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    string message = Console.ReadLine();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Enter the shift:");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    int shift = int.Parse(Console.ReadLine());
+                    CaesarCipher cc = new CaesarCipher();
+                    string EncryptedMessage = cc.Encrypt(message, shift);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Your encrypted message is: " + EncryptedMessage);
+                    Console.WriteLine("==================================================");
+                    goto Start;
+                }
+                else if (key.KeyChar == 'D' || key.KeyChar == 'd')
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nWelcome to decryption!");
+                    Console.WriteLine("Enter the directory that your encrypted file is in it:");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    string dir = @Console.ReadLine();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Enter your encrypted file' name:");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    string fileName = Console.ReadLine();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Enter the shift of decryption:");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    int shift = int.Parse(Console.ReadLine());
+                    CaesarCipher cc = new CaesarCipher();
+                    StreamReader sr = new StreamReader(dir + fileName);
+                    string DecryptedMessage = cc.Decrypt(sr.ReadToEnd(), shift);
+                    sr.Close();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Your decrypted message is: " + DecryptedMessage);
+                    Console.WriteLine("==================================================");
+                    goto Start;
+                }
+                else if (key.KeyChar == 'F' || key.KeyChar == 'f')
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nGood Bye! Hope to see you later!");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nThe input was invalid! Try again.");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("==================================================");
+                    goto Start;
+                }
+            }//try
+            catch(FormatException e)
+            {
+                Console.ForegroundColor=ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("==================================================");
+                goto Exception;
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("==================================================");
+                goto Exception;
+            }
+            Console.ReadKey();
+        }
+    }
+}
